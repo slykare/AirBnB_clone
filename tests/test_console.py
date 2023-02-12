cd2860dae74ae0cfa2fd3fb6ffcf1558eba79428
Robert Njoroge <97443055+RobertNjoroge@users.noreply.github.com>
2023-02-12 14:39:01 +0300

    Update test_console.py

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 """Defines unittests for console.py.
-
 Unittest classes:
     TestHBNBCommand_prompting
     TestHBNBCommand_help
@@ -1568,4 +1567,3 @@ class TestHBNBCommand_count(unittest.TestCase):
 
 if __name__ == "__main__":
     unittest.main()
-
